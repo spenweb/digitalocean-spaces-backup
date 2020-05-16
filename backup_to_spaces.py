c5387f149dbaad8f00776c1ae10696d3c5b1f9d2
Spencer Brown <git@spencerwebservices.com>
2020-05-16 12:44:58 -0700

    Zip directories to a timestamped file and clean up after upload

--- backup_to_spaces.py
+++ backup_to_spaces.py
@@ -1,9 +1,13 @@
 import os
 import boto3
+from zipfile import ZipFile
+from os.path import basename
+from time import gmtime, strftime
 
 SPACES_KEY = os.getenv('SPACES_KEY')
 SPACES_SECRET = os.getenv('SPACES_SECRET')
 BUCKET_NAME = 'humhub-cfb03fc7'
+PREFIX_SEPARATOR = '____'
 
 session = boto3.session.Session()
 client = session.client('s3',
@@ -26,16 +30,41 @@ def does_bucket_exist(name):
 
 
 def upload_file(filename, bucket):
-    file = open(filename, 'rb')
-    client.upload_fileobj(file, bucket, filename)
-    file.close()
+    with open(filename, 'rb') as file:
+        client.upload_fileobj(file, bucket, filename)
+
+
+def zip_folders(zip_name: str, folders_to_zip: list, base_dir: str):
+    with ZipFile(zip_name, 'w') as zip_object:
+        for dir_name in folders_to_zip:
+            for folder_name, subfolders, file_names in os.walk(os.path.join(base_dir, dir_name)):
+                for file_name in file_names:
+                    file_path = os.path.join(folder_name, file_name)
+                    zip_object.write(file_path, basename(file_path))
+
+
+def date_prefix_name(name):
+    date = strftime("%Y-%-m-%d", gmtime())
+    return f'{date}{PREFIX_SEPARATOR}{name}';
+
+
+def clean_up(zip_name: str):
+    os.remove(zip_name)
 
 
 def main():
+    zip_name = date_prefix_name("test.zip")
+    dirs_to_zip = ["test_dir"]
+    base_dir = os.getcwd()
+
     if not does_bucket_exist(BUCKET_NAME):
         create_bucket(BUCKET_NAME)
 
-    upload_file("test2.txt", BUCKET_NAME)
+    zip_folders(zip_name, dirs_to_zip, base_dir)
+
+    upload_file(zip_name, BUCKET_NAME)
+
+    clean_up(zip_name)
 
 
 if __name__ == "__main__":
